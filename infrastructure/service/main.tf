46b8dfd772780e8cdbd6f458a8cf0e2a26220f0c
kenko <kyoshitomi.cursor@gmail.com>
2023-11-26 21:27:15 +0900

    Create the microservice with Lambda functions

--- infrastructure/service/main.tf
+++ infrastructure/service/main.tf
@@ -7,8 +7,25 @@ module "sns" {
 }
 
 module "sqs" {
-  source = "../modules/sqs"
+  source  = "../modules/sqs"
   commons = local.commons
-  name = "notification"
-  sns = module.sns
+  name    = "notification"
+  sns     = module.sns
+}
+
+module "lambda" {
+  source  = "../modules/lambda"
+  commons = local.commons
+  name    = "driver_sally"
+  policy_statements = [
+    {
+      "Effect" : "Allow",
+      "Action" : [
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+      ],
+      "Resource" : "*"
+    }
+  ]
 }
